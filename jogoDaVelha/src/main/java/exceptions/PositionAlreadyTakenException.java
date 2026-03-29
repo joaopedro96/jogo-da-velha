e9d677d9368b5f3e89151fc555328e9577743234
Jadeilton <jadeiltondm@gmail.com>
2026-03-29 16:10:19 -0300

    Adicionando Exceptions

---
+++ jogoDaVelha/src/main/java/exceptions/PositionAlreadyTakenException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class PositionAlreadyTakenException extends Exception {
+
+
+
+
+
+    public PositionAlreadyTakenException(String message) {
+        super(message);
+    }
+}
